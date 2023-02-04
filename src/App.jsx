c026532dae8be6f2237a55e620e043daee4391ae
Fola <80335856+oMoFoLaYaN@users.noreply.github.com>
2023-02-04 03:00:59 +0100

    initialized files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+ 
+
+  return (
+    <div className="w-screen h-screen bg-red-300">
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default App
